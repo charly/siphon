30096c3f935dd03294ad5abd3e76668bad796d7b
Charles Sistovaris <charlysisto@gmail.com>
2013-12-05 16:11:33 +0100

    added the splat operator so nil arg equals no arg

--- lib/siphon/base.rb
+++ lib/siphon/base.rb
@@ -2,6 +2,12 @@ module Siphon
   # ========
   #   Base
   # ========
+  #
+  # use : Handles the ActiveRelation on which
+  #       scopes from a **FormObject** will be applied
+  #
+  # e.g : Siphon::Base.new(Book.published).scope(BookForm.new(params[:q]))
+  #
   class Base
     attr_accessor :relation
 
@@ -11,10 +17,10 @@ module Siphon
     end
 
     def scope(formobject)
-      scopeobject = Siphon::Adapter.new(formobject).call
+      scopes_hash = Siphon::Adapter.new(formobject).call
 
-      scopeobject.each do |meth, arg|
-        self.relation = relation.public_send(meth, arg)
+      scopes_hash.each do |meth, arg|
+        self.relation = relation.send(meth, *arg)
       end
 
       relation
